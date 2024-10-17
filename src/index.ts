319af7f6a344b030385a6fc0a4a71cf26ebb9321
IgorSasaki <igor-sasaki@hotmail.com>
2024-10-17 20:56:00 -0300

    feat: integrate library and middleware to error handling

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express'
+import 'express-async-errors'
 
+import ensureErrors from './middlewares/ensureErrors'
 import AppRouter from './routes/index.routes'
 
 const app = express()
 
+app.use(express.json())
 app.use(AppRouter)
+
+app.use(ensureErrors)
 
 app.listen(3333, () => console.log('Server started on 3333 🚀🚀'))
